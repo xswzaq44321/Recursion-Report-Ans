fe3f62c3bb822b115bcc6db74a7bee5423a44f86
Weber <xswzaq44321@gmail.com>
2018-10-24 02:14:46 +0800

    Add lil_practice

---
+++ lil_pract.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void explore(int layer){
+	static int nthExplore = 0;
+	printf("I'm in layer %d!\n", layer);
+	++nthExplore;
+	// if you are not experienced enough, don't go below 5.
+	if(layer == 5 && nthExplore < 20){
+		printf("I might better not go below layer 5...\n");
+		return;
+	}else if(layer == 10){
+		printf("Hits the bottom!\n");
+		return;
+	}
+	explore(layer + 1);
+}
+
+int main(){
+	for(int i = 0; i < 4; ++i){
+		explore(1);
+	}
+}
